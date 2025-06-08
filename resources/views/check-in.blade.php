4761d2903f83eb917f653d968a8df42fe1471867
alexander <alexander@uniquephoto.com>
2025-06-08 08:34:24 -0400

    Pre upgrade twaeks that i know work

--- resources/views/check-in.blade.php
+++ resources/views/check-in.blade.php
@@ -23,12 +23,8 @@
     </tbody>
 </table>
 <script>
-
-    $(function () {
-        $('#{{ $id }}').DataTable(
-                {!! $options !!}
-        );
-
+$(function () {
+    $('#{{ $id }}').DataTable({!! $options !!});
 
         var table = $('#{{ $id }}').DataTable();
 
@@ -37,35 +33,42 @@
         });
 
         $('#{{ $id }} tbody').on('click', 'button', function (event) {
-
             var data = table.row($(this).parents('tr')).data();
             var queryString = $.param({ids: data[data.length - 1], email: data[0]});
             var row = table.row($(this).parents('tr')).node();
-            //  alert(event.target.id+" and "+$(event.target).attr('class')+" and "+ data[0]);
 
             if (event.target.id == 'attended') {
                 $.get("/api/check-in?checkIn=yes&" + queryString, function (resp, status) {
                     console.log(resp);
                     if (resp['status'] == '200') {
                         $.admin.toastr.success(resp['message'], '', {positionClass: "toast-top-center"});
-                        $(event.target).tooltip("hide")
+                        $(event.target).tooltip("hide");
                         $(event.target).remove();
-                        table.cell(row, 5).data(0).draw();
-                        table.cell(row, 7).data(0).draw();
-                        //   $.pjax.reload('#pjax-container');
+                    table.cell(row, 5).data(0).draw(); // Update Owed Class Count
+                    table.cell(row, 7).data(0).draw(); // Update Options
+                    // Reload DataTable to reflect fresh cache
+                    $.get('/api/refresh-expo-cache', function(newData) {
+                        table.clear().rows.add(newData).draw();
+                    });
                     } else {
                         $.admin.toastr.error(resp['message'], '', {positionClass: "toast-top-center"});
                     }
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                console.error("Check-in request failed:", textStatus, errorThrown);
+                $.admin.toastr.error("Check-in failed: " + textStatus, '', {positionClass: "toast-top-center"});
                 });
             } else if (event.target.id == 'print') {
                 $.get("/api/check-in?print=print&" + queryString, function (resp, status) {
                     console.log(resp);
-                    //  alert(resp.message);
                     if (resp['status'] == 200) {
                         $.admin.toastr.success(resp['message'], '', {positionClass: "toast-top-center"});
+                    $(event.target).prop('disabled', true); // Disable print button
                     } else {
                         $.admin.toastr.error(resp['message'], '', {positionClass: "toast-top-center"});
                     }
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                console.error("Print request failed:", textStatus, errorThrown);
+                $.admin.toastr.error("Print failed: " + textStatus, '', {positionClass: "toast-top-center"});
                 });
             }
         });
